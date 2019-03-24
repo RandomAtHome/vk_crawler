6faa4ee630bad91b9038ae67d0f201629298e727
Fedor Chukharev <fedor.s.chukharev@gmail.com>
2019-03-25 02:09:58 +0300

    Implement search of matching users

--- config.py
+++ config.py
@@ -1,7 +1,9 @@
 # -*- coding: utf-8 -*-
 from vk_api import VkUserPermissions
+from datetime import date
 
 SCOPE = (VkUserPermissions.OFFLINE | VkUserPermissions.GROUPS)
 LOGIN_ATTEMPTS_LIMITS = 3
 APP_ID = 6912112
 MEMBER_LIMIT = 100
+CURRENT_DATE = date.today()
--- main.py
+++ main.py
@@ -27,10 +27,41 @@ def get_small_groups_iter(vk_session):
     :param vk_session: current session
     :return: iterator over groups with not more than 100 members
     """
-    tools = vk_api.tools.VkTools(vk_session)
-    return filter(lambda x: x["members_count"] <= config.MEMBER_LIMIT, tools.get_all_iter(method="groups.get", values={
+    def match(group):
+        return group["members_count"] <= config.MEMBER_LIMIT
+
+    tools = vk_api.VkTools(vk_session)
+    return filter(match, tools.get_all_iter(method="groups.get", values={
         "extended": 1,
         "fields": "members_count"
+    }, max_count=1000))
+
+
+def get_matching_users_iter(vk_session, group):
+    """
+    Take current session and vk group object, get iterator over all users in group and then filter them
+    :param vk_session: current session
+    :param group: vk object that describes group's parameters
+    :return: iterator over matched users in given group
+    """
+    def match(user):
+        if any(key not in user for key in ("sex", "bdate")):
+            return False
+        if user["sex"] != 1:
+            return False
+        date_elems = [int(i) for i in user["bdate"].split(".")]  # bdate is stored or in DD.MM.YYYY format, or in DD.MM
+        if len(date_elems) != 3:
+            return False
+
+        years_old = config.CURRENT_DATE.year - date_elems[2]
+        years_old -= ((config.CURRENT_DATE.month, config.CURRENT_DATE.day) < (date_elems[1], date_elems[0]))
+        return 20 <= years_old <= 22
+
+    tools = vk_api.VkTools(vk_session)
+    group_id = group["id"]
+    return filter(match, tools.get_all_iter(method="groups.getMembers", values={
+        "group_id": group_id,
+        "fields": "sex,bdate"
     }, max_count=1000))
 
 
@@ -44,9 +75,17 @@ def main():
         print("Failed to login!")
         return
 
+    groups = list(get_small_groups_iter(vk_session))  # to avoid further requests to server
     print("Names of groups:")
-    for group in get_small_groups_iter(vk_session):
+    for group in groups:
         print(group["name"])
+    print()
+    print("Names of ladies:")
+    for group in groups:
+        print(group["name"])
+        for user in get_matching_users_iter(vk_session, group):
+            print(user["last_name"], user["first_name"])
+        print("=====")
 
 
 if __name__ == '__main__':
